751e11a2e9cd1abcd10e68db234cdf4993adcdb9
Maxx Scholten <maxx@maxx-ubuntu-desktop.lan>
2020-08-24 12:15:04 -0400

    Fix wsUrl env var

--- plugins/native-websocket.js
+++ plugins/native-websocket.js
@@ -3,5 +3,6 @@ import Vue from 'vue'
 
 export default ({ store }, inject) => {
   console.log(process.env.wsUrl)
+  console.log(process.env.PORT)
   Vue.use(VueNativeSock, process.env.wsUrl, { store })
 }
