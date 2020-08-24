19768fb7214f8208208b0c44106eb3b0727f93a8
Maxx Scholten <maxx@maxx-ubuntu-desktop.lan>
2020-08-24 11:32:17 -0400

    Fix wsUrl env var

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -150,7 +150,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {}
+      env: {"wsUrl":"ws://localhost:3001"}
     }
     // Only set once
     if (!process.static && context.req) {
--- plugins/native-websocket.js
+++ plugins/native-websocket.js
@@ -2,5 +2,6 @@ import VueNativeSock from 'vue-native-websocket'
 import Vue from 'vue'
 
 export default ({ store }, inject) => {
-  Vue.use(VueNativeSock, process.env.WS_URL, { store })
+  console.log(process.env.wsUrl)
+  Vue.use(VueNativeSock, process.env.wsUrl, { store })
 }
